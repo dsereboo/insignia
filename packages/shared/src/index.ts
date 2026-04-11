ce5c99f4e3451d67f026a34a8554e4fdf46205ae
dsereboo <dsereboo@gmail.com>
2026-04-11 15:42:24 +0000

    dc-1: base setup for http client

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,3 @@
+export { HTTPClient } from './client/api'
+export type { RequestConfig, HttpResponse } from './client/api'
+export type { HTTPMethods } from './types'
---
+++ packages/telegram/src/constants.ts
@@ -0,0 +1,3 @@
+export const MethodNames ={
+    
+} as const
